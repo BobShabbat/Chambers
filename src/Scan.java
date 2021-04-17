bd63775d5e0de8983c087e61910d24591e7a3bf9
BobShabbat <clanwu36@gmail.com>
2021-04-17 16:24:13 +0300

    first commit

---
+++ src/Scan.java
@@ -0,0 +1,9 @@
+//import java.unil.Scanner;
+//
+//
+//public class Scan {
+//    public static void main (String [] args){
+//        Scanner str = new Scanner (System.in);
+//        System.out.println
+//    System.out.println();
+//}
